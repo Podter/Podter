7c3b620fad9bc7e5d91f97fe6d550bd5a67aa65a
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-10-11 14:23:27 +0700

    added assets

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,8 @@ export function Header() {
     <Div align="center">
       <a href="https://podter.me" target="_blank">
         <Image
-          srcLight="https://raw.githubusercontent.com/Podter/podter.me/main/public/og.png"
-          srcDark="https://raw.githubusercontent.com/Podter/podter.me/main/public/og.png"
+          srcLight="https://raw.githubusercontent.com/Podter/Podter/main/assets/podter-light.png"
+          srcDark="https://raw.githubusercontent.com/Podter/Podter/main/assets/podter-dark.png"
           alt="podter._"
         />
       </a>
